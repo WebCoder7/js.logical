aef7772ea647522b5d911fd34b17adab0ed51553
WebCoder7 <gulomovgulomjon296@gmail.com>
2023-03-31 20:05:57 +0500

    logo

---
+++ index.js
@@ -0,0 +1,37 @@
+// 5 - misol
+
+// let a = prompt("Birinchi son")
+// let b = prompt("Ikkinchi son")
+// let c = prompt("Uchinchi son")
+
+// function finLOGO(a, b, c,) {
+//   return (a * b * c)
+// }
+
+// console.log(finLOGO(a, b, c,));
+
+
+// function text(a) {
+//     return a + a + a
+// }
+// console.log(text("Ism"));
+
+
+
+
+
+
+// 6- misol
+// function FunctionA(n,b) {
+//   if (n<b) {
+//       return "<"
+//   }else if (n>b){
+//       return ">" 
+//   }else{
+//       return "="
+//   }
+// }
+// console.log(FunctionA([2],[2]));
+
+
+//   - misol
